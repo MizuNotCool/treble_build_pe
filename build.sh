ba66c53e7d42aa77f53604bc46994c982cf9d493
crazyads69 <111816472+crazyads69@users.noreply.github.com>
2022-08-29 19:14:43 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -35,7 +35,7 @@ installRequiredDependency() {
                 lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-gtk3-dev imagemagick git \
                 lunzip lzop schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk python2 perl  \
                 xmlstarlet virtualenv xz-utils rr jq libncurses5 pngcrush lib32ncurses5-dev git-lfs libxml2 \
-                openjdk-11-jdk-headless repo
+                openjdk-11-jdk-headless
 
     echo
 
