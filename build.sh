b5230cef2464ce061992b265831f7f7a0cf9a542
ItzKaguya <106611054+MizuNotCool@users.noreply.github.com>
2022-11-13 09:52:19 +0800

    pe

--- build.sh
+++ build.sh
@@ -28,29 +28,6 @@ export KBUILD_BUILD_HOST=SuzuNetwork-CI
 [ "$BRANCH" == "twelve" ] && BUILD="PixelExperience" || BUILD="PixelExperience_Plus"
 [ "$BRANCH" == "twelve" ] && PEMK="$BL/pe.mk" || PEMK="$BL/peplus.mk"
 
-installRequiredDependency() {
-    echo "---> Install Required Dependency"
-    sudo apt install -y bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf libxml2 \
-                lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk3.0-gtk3-dev imagemagick git \
-                lunzip lzop schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk python2 perl  \
-                xmlstarlet virtualenv xz-utils rr jq libncurses5 pngcrush lib32ncurses5-dev git-lfs libxml2 \
-                openjdk-11-jdk-headless
-
-    echo
-
-    sudo apt install -y openjdk-8-jdk apache2 bc bison build-essential ccache curl \
-                flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev \
-                lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev \
-                libssl-dev libxml2 libxml2-utils lzop pngcrush rsync \
-                schedtool squashfs-tools xsltproc zip zlib1g-dev git-core gnupg flex \
-                bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib \
-                libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache \
-                libgl1-mesa-dev libxml2-utils xsltproc unzip python3 libncurses5
-
-    echo
-
-}
-
 initRepos() {
     if [ ! -d .repo ]; then
         echo "--> Initializing PE workspace"
@@ -124,7 +101,6 @@ generatePackages() {
 START=`date +%s`
 BUILD_DATE="$(date +%Y%m%d)"
 
-installRequiredDependency
 initRepos
 syncRepos
 applyPatches
